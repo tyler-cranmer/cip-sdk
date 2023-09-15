4c8c4a0fd06312345f596e2d93524f993da1ec92
tyler-cranmer <tyler.cranmer@outlook.com>
2023-09-15 14:49:38 -0600

    fixing certain bugs

--- src/classes/cips.ts
+++ src/classes/cips.ts
@@ -6,7 +6,7 @@ import {
   bioAbi,
   nameSpaceAbi,
   erc721ABI2,
-  subprotocolRegistryAbi
+  subprotocolRegistryAbi,
 } from '../abi/abi';
 import {
   CID_NFT_CONTRACT,
@@ -14,7 +14,7 @@ import {
   BIO_CONTRACT,
   NAMESPACE_CONTRACT,
   PFP_WRAPPER_CONTRACT,
-  SUBPROTOCOL_REGISTRY_CONTRACT
+  SUBPROTOCOL_REGISTRY_CONTRACT,
 } from '../constants';
 import { NameSpace, ProfilePictureData, ProfilePictureInfo } from '../types';
 import { fetchImage, fontTransformer, transformURI } from '../lib';
@@ -29,9 +29,11 @@ export class CIP {
   bioContract: ethers.Contract;
   pfpContract: ethers.Contract;
   subprotocolCrontract: ethers.Contract;
-  
 
-  constructor(provider: ethers.Provider, web3Provider?: ethers.BrowserProvider ) {
+  constructor(
+    provider: ethers.Provider,
+    web3Provider?: ethers.BrowserProvider
+  ) {
     this.provider = provider;
     this.web3Provider = web3Provider;
     this.identityContract = new ethers.Contract(
@@ -55,64 +57,72 @@ export class CIP {
       pfpWrapperAbi,
       this.provider
     );
-    if(web3Provider){
+    if (web3Provider) {
       // @ts-ignore
-      this.signer = web3Provider.getSigner()
-      this.subprotocolCrontract = new ethers.Contract(SUBPROTOCOL_REGISTRY_CONTRACT, subprotocolRegistryAbi, this.signer);
+      this.signer = web3Provider.getSigner();
+      this.subprotocolCrontract = new ethers.Contract(
+        SUBPROTOCOL_REGISTRY_CONTRACT,
+        subprotocolRegistryAbi,
+        this.signer
+      );
     } else {
-      this.subprotocolCrontract = new ethers.Contract(SUBPROTOCOL_REGISTRY_CONTRACT, subprotocolRegistryAbi, this.provider);
+      this.subprotocolCrontract = new ethers.Contract(
+        SUBPROTOCOL_REGISTRY_CONTRACT,
+        subprotocolRegistryAbi,
+        this.provider
+      );
     }
 
-
     this.getBio = this.getBio.bind(this);
-    this.getNamespace = this.getNamespace.bind(this)
+    this.getNamespace = this.getNamespace.bind(this);
   }
 
-
   /**
-   * 
+   * @notice Register a new subprotocol. There is a 100 $NOTE fee when registering
+   *  @dev The options ordered, primary, active are not mutually exclusive. In practice, only one will be set for most subprotocols,
+   *  but if a subprotocol for instance supports int keys (mapped to one value) and a list of active NFTs, ordered and active is true.
    * @param ordered Ordering allows integers to be used as map keys, to one and only one value
    * @param primary Primary maps to zero or one value
    * @param active Subprotocols that have a list of a active NFTs
    * @param address Name of the subprotocol, has to be unique
    * @param name Address of the subprotocol NFT.
    * @param fee Fee (in $NOTE) for minting a new token of the subprotocol. Set to 0 if there is no fee. 10% is subtracted from this fee as a CID fee
-   * @returns 
-   * 
-   * 
-   * 
-   * 
-   *     /// @notice Register a new subprotocol. There is a 100 $NOTE fee when registering
-    /// @dev The options ordered, primary, active are not mutually exclusive. In practice, only one will be set for most subprotocols,
-    /// but if a subprotocol for instance supports int keys (mapped to one value) and a list of active NFTs, ordered and active is true.
-    /// @param _ordered Ordering allows integers to be used as map keys, to one and only one value
-    /// @param _primary Primary maps to zero or one value
-    /// @param _active Subprotocols that have a list of a active NFTs
-    /// @param _name Name of the subprotocol, has to be unique
-    /// @param _nftAddress Address of the subprotocol NFT.
-    /// @param _fee Fee (in $NOTE) for minting a new token of the subprotocol. Set to 0 if there is no fee. 10% is subtracted from this fee as a CID fee
+   * @returns
    */
-  public async registerSubprotocol(ordered: boolean, primary: boolean, active: boolean, address: string, name: string, fee: BigInt) {
+  public async registerSubprotocol(
+    ordered: boolean,
+    primary: boolean,
+    active: boolean,
+    address: string,
+    name: string,
+    fee: BigInt
+  ) {
     if (this.web3Provider) {
       try {
-        const tx = await this.subprotocolCrontract.register(ordered, primary, active, address, name, fee);
+        const tx = await this.subprotocolCrontract.register(
+          ordered,
+          primary,
+          active,
+          address,
+          name,
+          fee
+        );
         const reciept = await tx.wait();
-        console.log("Transaction sent:", tx);
+        console.log('Transaction sent:', tx);
         return reciept;
       } catch (error) {
-        console.log("error:", error);
+        console.log('error calling register:', error);
       }
     } else {
       throw new Error(`Connect a Web3 provider like meta mask.`);
     }
   }
- 
 
   /**
    * getCID is a method that calls the Canto Identity registration contract and returns the CID NFT ID that is registered to the provided address.
    * The CID NFT ID can be used to get a users sub protocol NFT IDs.
    * @param address Address to query.
-   * @returns The CID NFT ID of the provided address. 
+   * @returns The CID NFT ID of the provided address.
    */
   public async getCID(address: string): Promise<BigInt> {
     try {
@@ -123,16 +133,15 @@ export class CIP {
     }
   }
 
-
-/**
- * getPrimaryData is a method that is used to get the users sub protocol NFT IDs of any subprotocol registered with the user's CID.
- * This method is used to return any of the users registered sub protocol NFT IDs.
- * The return value can be used to get a user's sub protocol data. i.e. namespace or bio information.
- * 
- * @param cid User's CID NFT ID - Can be obtained by using getCID method.
- * @param subprotocolName the sub protocol name to query. Current list of sub protocols [namespace, profilepicture, bio]
- * @returns subProtocol registered NFT ID.
- */
+  /**
+   * getPrimaryData is a method that is used to get the users sub protocol NFT IDs of any subprotocol registered with the user's CID.
+   * This method is used to return any of the users registered sub protocol NFT IDs.
+   * The return value can be used to get a user's sub protocol data. i.e. namespace or bio information.
+   *
+   * @param cid User's CID NFT ID - Can be obtained by using getCID method.
+   * @param subprotocolName the sub protocol name to query. Current list of sub protocols [namespace, profilepicture, bio]
+   * @returns subProtocol registered NFT ID.
+   */
   public async getPrimaryData(
     cid: BigInt,
     subprotocolName: string
@@ -167,9 +176,8 @@ export class CIP {
     }
   }
 
-
   /**
-   * getPfpCID is a method that is used to return a user's registered pfp NFT ID. 
+   * getPfpCID is a method that is used to return a user's registered pfp NFT ID.
    * the return value is used by getPfpData() to get a user's profile picture information.
    * @param cid User's CID NFT ID. see getCID() for more information.
    * @returns User's pofile picture NFT ID
@@ -182,7 +190,6 @@ export class CIP {
       throw new Error(`Failed to get pfpCID from ${cid}.\nError: ${error}`);
     }
   }
-
 
   /**
    * getBioCID is a method that is used to return a user's registered bio NFT ID.
@@ -199,11 +206,11 @@ export class CIP {
     }
   }
 
-/**
- * getNamespace is a method that return a user's registered Namespace displayName and baseName.
- * @param namespaceCID User's registered namespace NFT ID. see getPrimaryData() or getNamespaceCID()
- * @returns Namespace Information in the form of display and base name. ex. displayName = 0xteewhy, baseName = 0xteewhy.canto
- */
+  /**
+   * getNamespace is a method that return a user's registered Namespace displayName and baseName.
+   * @param namespaceCID User's registered namespace NFT ID. see getPrimaryData() or getNamespaceCID()
+   * @returns Namespace Information in the form of display and base name. ex. displayName = 0xteewhy, baseName = 0xteewhy.canto
+   */
   public async getNamespace(namespaceCID: BigInt): Promise<NameSpace> {
     let displayName: string;
     let baseName: string;
@@ -230,7 +237,7 @@ export class CIP {
   /**
    * This method is used to get the ProfilePictureData to be used in the getPfpImage method.
    * @param pfpCID - pfpCID comes from the getPfpCid()
-   * @returns ProfilePictureData = [string, BigInt]. Profile Picture data is the NFT contract and ID of the users profile picture. 
+   * @returns ProfilePictureData = [string, BigInt]. Profile Picture data is the NFT contract and ID of the users profile picture.
    */
   public async getPfpData(pfpCID: BigInt): Promise<ProfilePictureData> {
     try {
@@ -246,10 +253,13 @@ export class CIP {
   }
 
   /**
-   * getPfpImage
-   * @param nftContractAddress 
-   * @param nftID 
-   * @returns 
+   * getPfpImage is a method that returns the profile prcture info/
+   * @param nftContractAddress NFT contract address of the pfp
+   * @param nftID NFT ID
+   * @returns ProfilePictureInfo = {
+   * src = link
+   * alt = nft name
+   * }
    */
   public async getPfpImage(
     nftContractAddress: string,
@@ -267,7 +277,7 @@ export class CIP {
       );
 
       const nftMeta: string = await nftContract.tokenURI(nftID);
-   
+
       if (nftMeta) {
         const transformedURI = transformURI(nftMeta);
         try {
@@ -288,11 +298,10 @@ export class CIP {
     }
   }
 
-
   /**
-   * getBio
-   * @param bioCID 
-   * @returns 
+   * getBio is a method that returns the bio associated with a CID
+   * @param bioCID bioCID. Comes from getBioCID()
+   * @returns a string form of the bio. 
    */
   public async getBio(bioCID: BigInt): Promise<string> {
     try {
@@ -306,65 +315,64 @@ export class CIP {
   }
 
   /**
-   * getByAddress
-   * @param address 
-   * @param subprotocolName 
-   * @param getter 
-   * @returns 
+   * getByAddress is a private helper method that is used for getBioByAddress, getNamespaceByAddress
+   * @param address user address
+   * @param subprotocolName the sub protocol name 
+   * @param getter callback function
+   * @returns
    */
-  private async getByAddress(address: string, subprotocolName: string, getter: (string: any) => Promise<any>): Promise<BigInt | null> {
-    try{
+  private async getByAddress(
+    address: string,
+    subprotocolName: string,
+    getter: (string: any) => Promise<any>
+  ): Promise<BigInt | null> {
+    try {
       const cid: BigInt = await this.getCID(address);
       if (cid == 0n) {
-        throw new Error(`Address: ${address} does not have a registered CID.`)
+        throw new Error(`Address: ${address} does not have a registered CID.`);
       } else {
         const subProtocol = await this.getPrimaryData(cid, subprotocolName);
         return getter(subProtocol);
       }
-    } catch (error){
-      throw new Error(
-        `Failed getByAddress: ${error}`
-      );
+    } catch (error) {
+      throw new Error(`Failed getByAddress: ${error}`);
     }
   }
 
-
   /**
-   * getBioByAddress
-   * @param address 
-   * @returns 
+   * getBioByAddress is a method that gets the users bio 
+   * @param address user address
+   * @returns the users bio in string format
    */
   public async getBioByAddress(address: string): Promise<any> {
     return await this.getByAddress(address, 'bio', this.getBio);
   }
 
   /**
-   * getNamespaceByAddress
-   * @param address 
-   * @returns 
+   * getNamespaceByAddress d
+   * @param address
+   * @returns
    */
   public async getNamespaceByAddress(address: string): Promise<any> {
-    return await this.getByAddress(address, 'namespace', this.getNamespace)
-
+    return await this.getByAddress(address, 'namespace', this.getNamespace);
   }
 
   /**
    * getPfpByAddress
-   * @param address 
-   * @returns 
+   * @param address
+   * @returns
    */
-  public async getPfpByAddress(address: string): Promise <any> {
-try {
-  const cid = await this.getCID(address);
-  const pfpCID = await this.getPfpCID(cid);
-  const [contract, id] = await this.getPfpData(pfpCID);
-  const pfp = await this.getPfpImage(contract, id);
-  return pfp;
-} catch(error){
-  throw new Error(
-    `Failed to get pfp data from address: ${address}.\n Error: ${error}`
-  );
-}
+  public async getPfpByAddress(address: string): Promise<any> {
+    try {
+      const cid = await this.getCID(address);
+      const pfpCID = await this.getPfpCID(cid);
+      const [contract, id] = await this.getPfpData(pfpCID);
+      const pfp = await this.getPfpImage(contract, id);
+      return pfp;
+    } catch (error) {
+      throw new Error(
+        `Failed to get pfp data from address: ${address}.\n Error: ${error}`
+      );
+    }
   }
-
 }
